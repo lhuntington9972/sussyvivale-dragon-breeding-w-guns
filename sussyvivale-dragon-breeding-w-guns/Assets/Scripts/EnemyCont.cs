2a77f5fa8af474cee81af3f793213be31d2c8bed
Trotter McLemore <tmclemore1344@student.pps.net>
2023-03-01 14:17:11 -0800

    YAY

--- sussyvivale-dragon-breeding-w-guns/Assets/Scripts/EnemyCont.cs
+++ sussyvivale-dragon-breeding-w-guns/Assets/Scripts/EnemyCont.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class EnemyCont : MonoBehaviour
 {
+    private int health;
+
+    private float moveSpeed;
+    private Rigidbody2D rb;
+
+    private Transform target;
+
+    private Vector2 moveDir;
+
     // Start is called before the first frame update
     void Start() {
-        
+        health = Random.Range(1, 5);
+        moveSpeed = 3;
+
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        moveDir = direction;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(moveDir.x, moveDir.y) * moveSpeed;
     }
 
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Bullet") {
-            Destroy(this.gameObject);
+            health -= 1;
+
+            if (health <= 0) {
+                Destroy(this.gameObject);
+            }
         }
     }
-} 
+}
